dd53fa8c9efccb60b126abff1c543445bc2e0c31
Влад Шипков <tsef47@mail.ru>
2021-01-12 17:32:55 +0300

    [+] Added Vuforia [+] Added test objects

---
+++ Assets/Scripts/LoginScreen.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LoginScreen : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI login;
+    [SerializeField]
+    private TextMeshProUGUI password;
+    [SerializeField]
+    private Button enterButton;
+    [SerializeField]
+    private Button registrarionButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enterButton.onClick.AddListener(TryEnter);
+        registrarionButton.onClick.AddListener(OpenRegistrationScreen);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void TryEnter()
+    {
+        string tempLogin = PlayerPrefs.GetString("login");
+        string tempPassword = PlayerPrefs.GetString("password");
+
+        if (tempLogin == login.ToString() && tempPassword == tempPassword.ToString())
+        {
+
+        }
+        else
+        {
+            OpenRegistrationScreen();
+        }
+    }
+
+    private void OpenRegistrationScreen()
+    {
+        Destroy(this);
+        Resources.Load<GameObject>("UIPanel/RegistrationPanel");
+    }
+}
---
+++ Assets/Scripts/RegistraionScreen.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class RegistraionScreen : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI login;
+    [SerializeField]
+    private TextMeshProUGUI password;
+    [SerializeField]
+    private TextMeshProUGUI tryPassword;
+    [SerializeField]
+    private Button registrarionButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        registrarionButton.onClick.AddListener(TryRegistration);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void TryRegistration()
+    {
+        if (password == tryPassword)
+        {
+            Debug.Log("login = " + login.ToString());
+            PlayerPrefs.SetString("login", login.ToString());
+            PlayerPrefs.SetString("password", password.ToString());
+            Destroy(this);
+            Resources.Load<GameObject>("UIPanel/LoginPanel");
+        }
+    }
+}
